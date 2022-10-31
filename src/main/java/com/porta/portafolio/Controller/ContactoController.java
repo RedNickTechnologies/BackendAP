eae59ffde36c0d09e08e14d0b5ace35536d5e1af
Nicolas <ngacevedo5@gmail.com>
2022-10-31 12:25:03 -0300

    Cambio de cors

--- src/main/java/com/porta/portafolio/Controller/ContactoController.java
+++ src/main/java/com/porta/portafolio/Controller/ContactoController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin ("https://nickacevedoportafolio.web.app/")
+@CrossOrigin // ("https://nickacevedoportafolio.web.app/")
 
 public class ContactoController {
     @Autowired
